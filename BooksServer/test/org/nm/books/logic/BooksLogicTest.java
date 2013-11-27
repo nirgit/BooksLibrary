ce3732769251ebcb3d7eb4f5d9027633cc94f230
nirgit <getnirm@gmail.com>
2013-11-27 18:06:52 +0200

    updating the tests

--- BooksServer/test/org/nm/books/logic/BooksLogicTest.java
+++ BooksServer/test/org/nm/books/logic/BooksLogicTest.java
@@ -1,8 +1,20 @@
 package org.nm.books.logic;
 
+import junit.framework.Assert;
+import org.easymock.EasyMock;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.nm.books.dal.BooksDAO;
+import org.nm.books.model.Book;
+import org.nm.books.model.BookId;
+import org.nm.books.model.BookLend;
+import org.nm.books.model.PersonId;
+import org.nm.books.model.dal.IBooksDAO;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * User: Nir Moav
@@ -12,11 +24,20 @@ import org.junit.Test;
  */
 public class BooksLogicTest {
 
+    private final static Book BOOK = new Book(new BookId("b-ID1"), "test book", "nir", 2013, new PersonId(11)) ;
     private BooksLogic logic ;
 
     @Before
     public void setUp() throws Exception {
-        this.logic = new BooksLogic(null) ;
+        IBooksDAO dao = createMockDao();
+        this.logic = new BooksLogic(dao) ;
+    }
+
+    private IBooksDAO createMockDao() {
+        List<Book> books = new ArrayList<Book>() ;
+        books.add(BOOK) ;
+
+        return new BooksDAO(books) ;
     }
 
     @After
@@ -26,17 +47,29 @@ public class BooksLogicTest {
 
     @Test
     public void testGetAllBooks() throws Exception {
-
+        List<Book> books = this.logic.getAllBooks();
+        Assert.assertNotNull(books);
     }
 
     @Test
     public void testGetAllAvailableBooks() throws Exception {
+        List<Book> allBooks = this.logic.getAllBooks() ;
+        List<Book> availBooks = this.logic.getAllAvailableBooks() ;
 
+        Assert.assertEquals(allBooks, availBooks);
     }
 
     @Test
     public void testGetAllBooksTakenByPerson() throws Exception {
+        BookLend lend = new BookLend(new BookId("b-ID1"), new PersonId(123));
+        this.logic.lendABook(lend);
 
+        List<Book> lentBooks = logic.getAllBooksTakenByPerson(new PersonId(456));
+        Assert.assertTrue(lentBooks.isEmpty());
+
+        lentBooks = logic.getAllBooksTakenByPerson(new PersonId(123)) ;
+        Assert.assertEquals(lentBooks.size(), 1);
+        Assert.assertEquals(lentBooks.get(0), BOOK);
     }
 
     @Test
