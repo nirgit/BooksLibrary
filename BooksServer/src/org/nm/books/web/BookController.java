ccbeee00390138c828bc6caf1e547f13fa9f8aab
nirgit <getnirm@gmail.com>
2013-12-01 00:16:12 +0200

    added json support as REST service

--- BooksServer/src/org/nm/books/web/BookController.java
+++ BooksServer/src/org/nm/books/web/BookController.java
@@ -1,8 +1,13 @@
 package org.nm.books.web;
 
+import org.nm.books.model.Book;
+import org.nm.books.model.BookId;
+import org.nm.books.model.PersonId;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * User: Nir Moav
@@ -13,8 +18,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class BookController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    @RequestMapping(value = "/", method = RequestMethod.GET)
     public String sayHello() {
         return "/home" ;
+    }
+
+    // return the json!
+    @RequestMapping("/book")
+    public @ResponseBody Book getBook(
+            @RequestParam(value="name", required=false, defaultValue="World") String name) {
+        return new Book(new BookId("111"), "Spring REST JSON", "Nir", 2013, new PersonId(23)) ;
     }
 }
