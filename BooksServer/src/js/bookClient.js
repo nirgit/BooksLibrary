2dbbbcd86920b3cc68633a785326f50f940a21b1
nirgit <getnirm@gmail.com>
2014-01-12 00:34:21 +0200

    Creation of the first generated View skeleton.

--- BooksServer/src/js/bookClient.js
+++ BooksServer/src/js/bookClient.js
@@ -20,3 +20,19 @@ function getAllBooks() {
         }
     }) ;
 }
+
+// Entry point of the app
+function init() {
+    var mainNode = document.getElementById("main") ;
+    var booksView = new views.BooksView(mainNode) ;
+    booksView.go() ;
+}
+
+function oldInit() {
+    getAllBooks() ;
+
+    $("#searchButton").on('click', function() {
+        var bookName = $("#bookNameToSearch").val() ;
+        getBook(bookName) ;
+    }) ;
+}
--- BooksServer/src/js/views/booksView.js
+++ BooksServer/src/js/views/booksView.js
@@ -5,6 +5,29 @@ define.Class("views.BooksView", function(def) {
     def.methods = {
         __init: function(node, args) {
             this.super(node, args) ;
+            this._createViewElements() ;
+        },
+
+        _createViewElements: function(){
+            this._title = this._createTitle() ;
+            this._bookList = this._createBookList() ;
+        },
+
+        _createTitle: function() {
+            var title = new Element("div") ;
+            title.innerHTML = "Library Books" ;
+            return title ;
+        },
+
+        _createBookList: function() {
+            var bookList = new Element("div", {width: "800px", height: "500px", position: "absolute", border: "4px solid purple"}) ;
+            bookList.innerHTML = "The actual book list..." ;
+            return bookList ;
+        },
+
+        _render: function(view) {
+            view.appendChild(this._title) ;
+            view.appendChild(this._bookList) ;
         }
     } ;
 }) ;
