dd181edfaad129193dc7fe0bf386153aacf16811
nirgit <getnirm@gmail.com>
2014-01-26 01:13:41 +0200

    some styling changes to the books in the list

--- BooksServer/src/js/views/BookView.js
+++ BooksServer/src/js/views/BookView.js
@@ -11,9 +11,12 @@ define.Class("views.BookView", function(def) {
             ['CLASS_OWNER_SUB_STYLE_NAME', "owner"],
             ['STYLES', {
                         'BOOK':         ".book {background: url(imgs/book.jpg) 214px 266px; color: white; float: left; font-family: sans-serif;" +
-                                        "font-size: 20px; height: 250px; width: 190px; margin: 10px; text-align: center; word-wrap: break-word;}",
+                                        "font-size: 20px; height: 250px; width: 190px; margin: 10px; text-align: center; word-wrap: break-word;" +
+                                        "box-shadow: 5px 5px 15px 2px gray;}",
                         'BOOK_TITLE':   ".book-title { color: cadetblue; font-family: sans-serif; font-size: 22px; font-weight: lighter;" +
-                                        "margin: 5px; text-align: center; word-wrap: break-word; }"
+                                        "margin: 5px; text-align: center; word-wrap: break-word; height: 50px}",
+                        'BOOK_YEAR':    ".book-year {font-size: 16px; font-family: serif;}",
+                        'BOOK_OWNER':   ".book-owner {font-size: 14px; position: relative; top: 20%}"
                      }
             ]
         ],
@@ -26,6 +29,8 @@ define.Class("views.BookView", function(def) {
                 var style = new Element("style") ;
                 style.innerHTML += this.STYLES.BOOK ;
                 style.innerHTML += this.STYLES.BOOK_TITLE ;
+                style.innerHTML += this.STYLES.BOOK_YEAR ;
+                style.innerHTML += this.STYLES.BOOK_OWNER ;
 
                 document.head.appendChild(style) ;
             },
