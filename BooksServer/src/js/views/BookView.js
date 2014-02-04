3662af1154a47ed0087c2eff49906050ed22df5c
nirgit <getnirm@gmail.com>
2014-02-05 00:29:11 +0200

    Fix for the BookView 'Skin' and it's parts.

--- BooksServer/src/js/views/BookView.js
+++ BooksServer/src/js/views/BookView.js
@@ -18,11 +18,19 @@ define.Class("views.BookView", function(def) {
                         'BOOK_YEAR':    ".book-year {font-size: 16px; font-family: serif;}",
                         'BOOK_OWNER':   ".book-owner {font-size: 14px; position: relative; top: 20%}"
                      }
-            ]
+            ],
+            ["$skin", null],
+            ['PARTS', {
+                        TITLE: "title",
+                        AUTHOR: "author",
+                        YEAR: "year",
+                        OWNER: "owner"
+            }]
         ],
         methods: {
             $init: function() {
                 this._createBookStyle() ;
+                this.$skin = this._createSkin() ;
             },
 
             _createBookStyle: function() {
@@ -34,6 +42,29 @@ define.Class("views.BookView", function(def) {
 
                 document.head.appendChild(style) ;
             },
+
+            _createSkin: function() {
+                var bookContainer       = new Element("div") ;
+                bookContainer.className = this.CLASS_STYLE_NAME ;
+                var title             = this._createBookHeader(this.PARTS.TITLE, this.CLASS_TITLE_SUB_STYLE_NAME) ;
+                var author            = this._createBookHeader(this.PARTS.AUTHOR, this.CLASS_AUTHOR_SUB_STYLE_NAME) ;
+                var year              = this._createBookHeader(this.PARTS.YEAR, this.CLASS_YEAR_SUB_STYLE_NAME) ;
+                var owner             = this._createBookHeader(this.PARTS.OWNER, this.CLASS_OWNER_SUB_STYLE_NAME) ;
+
+                bookContainer.appendChild(title) ;
+                bookContainer.appendChild(author) ;
+                bookContainer.appendChild(year) ;
+                bookContainer.appendChild(owner) ;
+
+                return bookContainer ;
+            },
+
+            _createBookHeader: function(partName, subStyleName) {
+                var element = new Element("div") ;
+                element.setAttribute("part", partName) ;
+                element.className = utils.createClassStyle(this.CLASS_STYLE_NAME, subStyleName) ;
+                return element ;
+            }
         }
     } ;
 
@@ -44,41 +75,28 @@ define.Class("views.BookView", function(def) {
         },
 
         getTitle: function() {
-            return this._title ;
+            return this._getPart(this.PARTS.TITLE) ;
         },
 
         getAuthor: function() {
-            return this._author ;
+            return this._getPart(this.PARTS.AUTHOR) ;
         },
 
         getYear: function() {
-            return this._year ;
+            return this._getPart(this.PARTS.YEAR) ;
         },
 
         getOwner: function() {
-            return this._owner ;
+            return this._getPart(this.PARTS.OWNER) ;
+        },
+
+        _getPart: function(partName) {
+            return this._node.querySelector("[part=" + partName + "]") ;
         },
 
         _render: function(node) {
-            var bookContainer       = new Element("div") ;
-            bookContainer.className = this.CLASS_STYLE_NAME ;
-            this._title             = this._createBookHeader(this.CLASS_TITLE_SUB_STYLE_NAME) ;
-            this._author            = this._createBookHeader(this.CLASS_AUTHOR_SUB_STYLE_NAME) ;
-            this._year              = this._createBookHeader(this.CLASS_YEAR_SUB_STYLE_NAME) ;
-            this._owner             = this._createBookHeader(this.CLASS_OWNER_SUB_STYLE_NAME) ;
-
-            bookContainer.appendChild(this._title) ;
-            bookContainer.appendChild(this._author) ;
-            bookContainer.appendChild(this._year) ;
-            bookContainer.appendChild(this._owner) ;
-
-            node.appendChild(bookContainer) ;
-        },
-
-        _createBookHeader: function(subStyleName) {
-            var element = new Element("div") ;
-            element.className = utils.createClassStyle(this.CLASS_STYLE_NAME, subStyleName) ;
-            return element ;
+            var bookSkinInstance = this.$skin.cloneNode(true) ;
+            node.appendChild(bookSkinInstance) ;
         }
     }
 }) ;
