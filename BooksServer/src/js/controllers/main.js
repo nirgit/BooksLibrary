9a54f618a3875bdd4513cdf59c09ee703446acf6
nirgit <getnirm@gmail.com>
2014-04-14 16:52:07 +0300

    Deleting obsolete views & controllers and replacing them with a partials and an angular routing module

---
+++ BooksServer/src/js/controllers/main.js
@@ -0,0 +1,15 @@
+getApp().
+    config(['$routeProvider',
+    function($routeProvider) {
+        $routeProvider.when('/', {
+            templateUrl: 'partials/main.html',
+            controller: 'myCtrlr'
+        }).
+        otherwise({
+            redirectTo: '/'
+        })
+    }]);
+
+getApp().controller('myCtrlr', function($scope) {
+    $scope.myName = "Nir M." ;
+}) ;
