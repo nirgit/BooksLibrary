d66f2b80644476768503a40e13fa7e1c128359ca
nirgit <getnirm@gmail.com>
2014-05-13 01:20:40 +0300

    Creation of the books (factory) model.

--- BooksServer/src/js/controllers/booksController.js
+++ BooksServer/src/js/controllers/booksController.js
@@ -1,13 +1,9 @@
-getApp().controller('booksController', ['$scope', function ($scope) {
-    $scope.books = [
-        {'name': 'Alice in Wonderland',         'author': 'Louis Carol',    'year': '1886'},
-        {'name': 'Man\'s search for Meaning',   'author': 'Harold Fein',    'year': '2004'},
-        {'name': 'Javascript - The good parts', 'author': 'Oreilly',        'year': '2008'}
-    ] ;
+getApp().controller('booksController', ['$scope', 'booksFactory', function ($scope, booksFactory) {
+    $scope.books = booksFactory.getBooks() ;
 
     $scope.addBook = function() {
         var bookToInsert = $scope.newBook ;
-        $scope.books.push(bookToInsert) ;
+        booksFactory.getBooks().push(bookToInsert) ;
         $scope.newBook = {} ;
     }
 }]) ;
---
+++ BooksServer/src/js/model/booksModel.js
@@ -0,0 +1,14 @@
+getApp().factory('booksFactory', function() {
+    var books = [
+        {'name': 'Alice in Wonderland',         'author': 'Louis Carol',    'year': '1886'},
+        {'name': 'Man\'s search for Meaning',   'author': 'Harold Fein',    'year': '2004'},
+        {'name': 'Javascript - The good parts', 'author': 'Oreilly',        'year': '2008'}
+    ] ;
+
+    var factory = {} ;
+    factory.getBooks = function getBooks() {
+        return books ;
+    } ;
+
+    return factory ;
+}) ;
